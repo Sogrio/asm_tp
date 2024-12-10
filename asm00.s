33a992776443b0da663a264633121ee9671a0040
Sogrio <95721424+Sogrio@users.noreply.github.com>
2024-12-10 12:49:09 +0100

    Delete asm00.s

--- asm00.s
+++
@@ -1,8 +0,0 @@
-section .text
-    global _start
-
-_start:
-    xor rdi, rdi          ; Met RDI (argument de sortie) à 0
-    mov rax, 60           ; Appel système pour terminer le programme (sys_exit)
-    syscall               ; Effectue l'appel système
-
